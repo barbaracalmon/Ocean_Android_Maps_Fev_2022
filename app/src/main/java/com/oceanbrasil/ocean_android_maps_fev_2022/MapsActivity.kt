01bf318dee8d82d0d16337fc1f2e4516b8eb5890
dj <dj@ime.usp.br>
2022-02-03 12:01:40 -0300

    Versão final

--- app/src/main/java/com/oceanbrasil/ocean_android_maps_fev_2022/MapsActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_maps_fev_2022/MapsActivity.kt
@@ -1,15 +1,23 @@
 package com.oceanbrasil.ocean_android_maps_fev_2022
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
 import com.oceanbrasil.ocean_android_maps_fev_2022.databinding.ActivityMapsBinding
+import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -37,12 +45,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
+        val crato = LatLng(-7.24045, -39.41875)
+        val crato2 = LatLng(-17.24045, -19.41875)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(MarkerOptions().position(crato).title("Marker in Crato-CE"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(crato,10.0f))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(crato,6.0f))
+
+
+        mMap.addCircle(
+            CircleOptions()
+                .center(crato)
+                .radius(200.0)
+                .strokeColor(Color.GRAY)
+                .fillColor(Color.LTGRAY)
+                .strokeWidth(10f)
+        )
+
+        mMap.addPolygon(
+            PolygonOptions().add(crato).add(sydney).add(crato2).add(crato)
+        )
     }
 }
